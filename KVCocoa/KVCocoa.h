e770439e45f9f711763214b967adf4e886d38a6c
Vivek <k-vivek.karuppanna@hpe.com>
2018-08-03 11:43:42 +0530

    Initial Commit

---
+++ KVCocoa/KVCocoa.h
@@ -0,0 +1,19 @@
+//
+//  KVCocoa.h
+//  KVCocoa
+//
+//  Created by Vivek on 03/08/2018.
+//  Copyright © 2018 HP Enterprise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KVCocoa.
+FOUNDATION_EXPORT double KVCocoaVersionNumber;
+
+//! Project version string for KVCocoa.
+FOUNDATION_EXPORT const unsigned char KVCocoaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KVCocoa/PublicHeader.h>
+
+
